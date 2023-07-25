f1557de4a9a5df1e53b02da10a8fb66bf171026c
smartchouchou <1716203353@qq.com>
2023-07-25 15:07:06 +0800

    fix: interface

--- src/components/HomeNav/HomeCommend/store/actionCreators.js
+++ src/components/HomeNav/HomeCommend/store/actionCreators.js
@@ -9,7 +9,7 @@ export const changeRecommendList = (data) =>({
 export const getRecommendList =() =>{
     return (dispatch) =>{
         getRecommendRequest().then(data=>{
-            dispatch(changeRecommendList(data))
+            dispatch(changeRecommendList(data.data))
             dispatch(changeArrive(true))
         })
     }
